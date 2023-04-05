2b9a30686f57845f2ab8fa63c94dcb9142038355
git_manasse8002 <manassemburu@gmail.com>
2023-04-05 22:13:19 +0300

    like button functionality

--- Sea food/index.js
+++ Sea food/index.js
@@ -1,30 +1,35 @@
-let likes=0
-document.addEventListener("DOMContentLoaded", ()=>{ 
-    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+let likes = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  const recipesDiv = document.getElementById("recipes");
+  fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
     .then(response => response.json())
     .then(data => {
-        let recipesDiv = document.getElementById("recipes");
-        data.meals.forEach(meal => {
-            let recipeDiv = document.createElement("div");
-            recipeDiv.className = "recipe";
-            recipeDiv.innerHTML = `
-                <h2>${meal.strMeal}</h2>
-                <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-                <p>Category: ${meal.strMeal}</p>
-                <p>Area: ${meal.idMeal}</p>
-                <button id="btn"> Like ${likes} </button>
-            `;
-            recipesDiv.appendChild(recipeDiv);
-            let button = document.getElementById("btn")
-            button.addEventListener("click",()=> {
-                likes=likes ++
-                console.log(likes)
-
-      }) 
+      data.meals.forEach(meal => {
+        const recipeDiv = document.createElement("div");
+        recipeDiv.className = "recipe";
+        const h2 = document.createElement("h2");
+        h2.textContent = meal.strMeal;
+        recipeDiv.appendChild(h2);
+        const img = document.createElement("img");
+        img.src = meal.strMealThumb;
+        img.alt = meal.strMeal;
+        recipeDiv.appendChild(img);
+        const categoryP = document.createElement("p");
+        categoryP.textContent = `Category: ${meal.strCategory}`;
+        recipeDiv.appendChild(categoryP);
+        const areaP = document.createElement("p");
+        areaP.textContent = `Area: ${meal.strArea}`;
+        recipeDiv.appendChild(areaP);
+        const button = document.createElement("button");
+        button.textContent = `Like ${likes}`;
+        button.addEventListener("click", () => {
+          likes++;
+          button.textContent = `Like ${likes}`;
+          console.log(likes);
         });
-        
+        recipeDiv.appendChild(button);
+        recipesDiv.appendChild(recipeDiv);
+      });
     })
-    .catch(error => console.log(error));  
-   
-})
-  
+    .catch(error => console.log(error));
+});
