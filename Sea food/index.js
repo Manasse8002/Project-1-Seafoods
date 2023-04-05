408ad587fbbd2314a48fe86720739ebd4b59d3b5
git_manasse8002 <manassemburu@gmail.com>
2023-04-05 21:41:31 +0300

    initial commits

--- Sea food/index.js
+++ Sea food/index.js
@@ -1,19 +1,30 @@
+let likes=0
+document.addEventListener("DOMContentLoaded", ()=>{ 
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+    .then(response => response.json())
+    .then(data => {
+        let recipesDiv = document.getElementById("recipes");
+        data.meals.forEach(meal => {
+            let recipeDiv = document.createElement("div");
+            recipeDiv.className = "recipe";
+            recipeDiv.innerHTML = `
+                <h2>${meal.strMeal}</h2>
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                <p>Category: ${meal.strMeal}</p>
+                <p>Area: ${meal.idMeal}</p>
+                <button id="btn"> Like ${likes} </button>
+            `;
+            recipesDiv.appendChild(recipeDiv);
+            let button = document.getElementById("btn")
+            button.addEventListener("click",()=> {
+                likes=likes ++
+                console.log(likes)
 
-  fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
-  .then(response => response.json())
-  .then(data => {
-      let recipesDiv = document.getElementById("recipes");
-      data.meals.forEach(meal => {
-          let recipeDiv = document.createElement("div");
-          recipeDiv.className = "recipe";
-          recipeDiv.innerHTML = `
-              <h2>${meal.strMeal}</h2>
-              <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-              <p>Category: ${meal.strMeal}</p>
-              <p>Area: ${meal.idMeal}</p>
-              <p><a href="https://www.themealdb.com/meal/${meal.idMeal}" target="_blank">View Recipe</a></p>
-          `;
-          recipesDiv.appendChild(recipeDiv);
-      });
-  })
-  .catch(error => console.log(error));
+      }) 
+        });
+        
+    })
+    .catch(error => console.log(error));  
+   
+})
+  
