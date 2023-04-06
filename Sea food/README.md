d92a80ce5897583fb67f722368ec5eda3af5ef36
git_manasse8002 <manassemburu@gmail.com>
2023-04-06 15:21:25 +0300

    styling my readme file

--- Sea food/README.md
+++ Sea food/README.md
@@ -1,2 +1,17 @@
 # Project-1-ph1
+## About
+### Sea foods
+This is a wall where you can see the different dishes of sea foods.
+I created it since I noted that in many of the menus in my place don't have sea foods displayed yet they cook and serve them.
+The purpose of the wall is to ensure that the hotels are more efficient since now they can have their menus online in soft copies hence making their work easier.
+## How to install and run my project
+
+## How to use my project
+
+## Credits
+
+## How to contribute to project
+
+## Tests for my project
+
 
--- Sea food/index.js
+++ Sea food/index.js
@@ -1,6 +1,8 @@
 let likes = 0;
 document.addEventListener("DOMContentLoaded", () => {
   const recipesDiv = document.getElementById("recipes");
+
+
   fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
     .then(response => response.json())
     .then(data => {
@@ -8,22 +10,27 @@ document.addEventListener("DOMContentLoaded", () => {
         const recipeDiv = document.createElement("div");
         recipeDiv.className = "recipe";
 
+
         const h2 = document.createElement("h2");
         h2.textContent = meal.strMeal;
         recipeDiv.appendChild(h2);
 
+        
         const img = document.createElement("img");
         img.src = meal.strMealThumb;
         img.alt = meal.strMeal;
         recipeDiv.appendChild(img);
 
+
         const categoryP = document.createElement("p");
         categoryP.textContent = `Category: ${meal.strCategory}`;
         recipeDiv.appendChild(categoryP);
 
+
         const areaP = document.createElement("p");
         areaP.textContent = `Area: ${meal.strArea}`;
         recipeDiv.appendChild(areaP);
+
 
         const button = document.createElement("button");
         button.textContent = `Like ${likes}`;
@@ -32,7 +39,8 @@ document.addEventListener("DOMContentLoaded", () => {
           button.textContent = `Like ${likes}`;
           console.log(likes);
         });
-        
+
+
         recipeDiv.appendChild(button);
         recipesDiv.appendChild(recipeDiv);
       });
